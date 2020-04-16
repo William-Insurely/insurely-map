7b0685da92efe289dcdf8b4ae9b275d4b1054aa9
William-Insurely <william@insurely.se>
2020-04-16 11:03:22 +0200

    added fetch code for api

--- src/Components/outputdata/index.js
+++ src/Components/outputdata/index.js
@@ -2,48 +2,64 @@ import React, { useEffect, useState } from "react";
 import { filterGeojson } from "../functions/script";
 import Success from "./success";
 import Error from "./error";
+import axios from 'axios';
 
-const InsuranceComapnys = [
-  {
-    name: "Trygg Hansa",
-    img:
-      "https://www.bilcentersyd.se/wp-content/uploads/2018/09/Trygg-Hansa.png",
-  },
-  {
-    name: "Länsförsäkringar",
-    img: "https://www.stickpng.com/assets/images/5a27cf3852b1cc0d022e6d68.png",
-  },
-  {
-    name: "If Försäkring",
-    img:
-      "https://via.tt.se/data/images/00158/5069fd52-cadf-4576-b267-b5dc79da2b69.png",
-  },
-  {
-    name: "Hedvig",
-    img:
-      "https://fatcamp.io/xn--frskring-2za1p.se/images/company-logos/hedvig-forsakring-logotyp.png",
-  },
-];
+// const InsuranceComapnys = [
+//   {
+//     name: "Trygg Hansa",
+//     img:
+//       "https://www.bilcentersyd.se/wp-content/uploads/2018/09/Trygg-Hansa.png",
+//   },
+//   {
+//     name: "Länsförsäkringar",
+//     img: "https://www.stickpng.com/assets/images/5a27cf3852b1cc0d022e6d68.png",
+//   },
+//   {
+//     name: "If Försäkring",
+//     img:
+//       "https://via.tt.se/data/images/00158/5069fd52-cadf-4576-b267-b5dc79da2b69.png",
+//   },
+//   {
+//     name: "Hedvig",
+//     img:
+//       "https://fatcamp.io/xn--frskring-2za1p.se/images/company-logos/hedvig-forsakring-logotyp.png",
+//   },
+// ];
 
-const OutputData = ({ zipcode,onSubmit }) => {
-  const [company, setCompany] = useState(null);
+const OutputData = ({ zipcode, onSubmit }) => {
+  // const [company, setCompany] = useState(null);
   const [data, setData] = useState(null);
-  const randomNumer = (max, min) => {
-    return Math.floor(Math.random() * max) + min;
-  };
+  const [insurleydata, setInsurleydata] = useState(true);
+
+  // const randomNumer = (max, min) => {
+  //   return Math.floor(Math.random() * max) + min;
+  // };
 
   useEffect(() => {
+
+    axios.get('https://api.insurely.se/insurance-map/11325')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+
     setData(filterGeojson(zipcode));
-    setCompany(InsuranceComapnys[randomNumer(4, 0)]);
+    // setCompany(InsuranceComapnys[randomNumer(4, 0)]);
   }, [zipcode]);
+  
+  
 
   return (
     <div className="outputdata-container">
-      {company && data.insurelydata ? (
+      {data ? (
         <>
           <Success
-            randomNumer={randomNumer}
-            company={company}
+            // randomNumer={randomNumer}
+            // company={company}
             zipcode={zipcode}
             suggestions={data.suggestions}
             onSubmit={onSubmit}
--- src/Components/outputdata/success.js
+++ src/Components/outputdata/success.js
@@ -2,19 +2,19 @@ import React from "react";
 import Suggestions from "./suggestions";
 import Button from '../button'
 
-const Success = ({ randomNumer, company, zipcode, suggestions, onSubmit }) => {
+const Success = ({ zipcode, suggestions, onSubmit }) => {
   return (
     <div className="success">
       <Suggestions suggestionsFromDB={suggestions} onSubmit={onSubmit} />
       <h2 className="success--zipcode">{zipcode}</h2>
       <div className="success--averageprice">
         <h2>Genomsnittpris:</h2>
-        <p>{randomNumer(250, 150)} sek / Månad</p>
+        <p>130 sek / Månad</p>
       </div>
 
       <div className="success--popularcompanys">
         <h2>Populärast försäkring: </h2>
-        <p>{company.name}</p>
+        <p>Hedvig</p>
       </div>
       <Button text='Byt Din Försäkring!'/>
     </div>
